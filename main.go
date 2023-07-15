030f3383fbad69d0cd29e4d40d470f894105f581
Ayan Maiti <100613640+moonman369@users.noreply.github.com>
2023-07-16 00:26:04 +0530

    Create basic server side code

--- main.go
+++ main.go
@@ -6,6 +6,37 @@ import (
 	"net/http"
 )
 
-func main  {
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() error: %v", err)
+		return
+	}
+	fmt.Fprintf(w, "POST request successful")
+	name, address := r.FormValue("name"), r.FormValue("address")
+	fmt.Fprintf(w, "Name = %v\n", name)
+	fmt.Fprintf(w, "Address = %v\n", address)
+}
 
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not Found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method is not allowed by the server", http.StatusMethodNotAllowed)
+		return
+	}
+	fmt.Fprint(w, "Hello Gophers!!")
+}
+
+func main() {
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Println("Starting GO server at port 8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
